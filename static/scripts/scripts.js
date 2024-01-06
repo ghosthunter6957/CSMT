9d5bbc3109e960a9c13aa9d3cfaf781be02d9227
ghosthunter6957 <46430092+ghosthunter6957@users.noreply.github.com>
2024-01-06 13:47:54 +0000

    added the drop down menu so users can quickly move between learning pages. Functionality added.

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -3,15 +3,14 @@ const usernameField = document.getElementById('inputContainer');
 //event listeners
 //variables
 function showdropdown(){
-  if (document.getElementById('master-dropdown-menu')){
-  }
   let master_dropdown = document.getElementById('master-dropdown-menu');
-  console.log(master_dropdown);
-  if (master_dropdown.classlist.contains("hidden")){
-      master_dropdown.classList.remove("hidden")
-      master_dropdown.classList.add("visible")
-  } else {
-      master_dropdown.classList.remove("visible")
-      master_dropdown.classList.add("hidden")
-       }
+  var classes = master_dropdown.className
+  if (classes.indexOf('hidden') > -1) {
+    master_dropdown.classList.remove("hidden")
+    master_dropdown.classList.add("visible")}
+  else {master_dropdown.classList.remove("visible")
+    master_dropdown.classList.add("hidden")}
+  
+     
+      
 }
